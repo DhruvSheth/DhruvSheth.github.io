0b1bf11cdb1ad0a895acba274bd728ce82d33f4f
Dhruv Sheth <dhruvnsheth@gmail.com>
2025-02-08 00:08:52 +0700

    Fix typescript config

---
+++ dist/platformer.js
@@ -0,0 +1,19 @@
+"use strict";
+const canvas = document.getElementById("platformerCanvas");
+if (!(canvas instanceof HTMLCanvasElement)) {
+    throw new Error("Element is not a canvas");
+}
+const ctx = canvas.getContext("2d");
+if (!ctx) {
+    throw new Error("Failed to get 2D context");
+}
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+// we want to respect the size of the window
+function handleResize() {
+}
+window.addEventListener('resize', handleResize);
+ctx.fillStyle = "black";
+ctx.font = `50px Courier New`;
+const helloWorld = `Hello World!`;
+ctx.strokeText(helloWorld, 10, 50);
--- platformer.ts
+++
@@ -1,7 +0,0 @@
-function greeter(person: string) {
-    return "Hello, " + person;
-  }
-   
-  let user = "dhruv";
-   
-  document.body.textContent = greeter(user);
